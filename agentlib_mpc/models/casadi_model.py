11f18dd887979aa14633dd520ca09f0e9514ac4b
ses <steffen.eser@eonerc.rwth-aachen.de>
2025-09-01 09:48:36 +0200

    try fix

--- agentlib_mpc/models/casadi_model.py
+++ agentlib_mpc/models/casadi_model.py
@@ -296,10 +296,11 @@ class CasadiModel(Model):
     parameters and override the setup_system() method."""
 
     config: CasadiModelConfig
+    _is_initialized = False
 
     def __init__(self, **kwargs):
         # Temporarily disable __setattr__ checks during initialization as self.config does not exists at first
-        super().__setattr__("_is_initialized", False)
+        # super().__setattr__("_is_initialized", False)
         # Initializes the config
         super().__init__(**kwargs)
         self._is_initialized = True
